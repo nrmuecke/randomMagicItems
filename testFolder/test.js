121b70eef1bf2ae8d9396c6b5e8de930da5fc6b4
Nathan Muecke <nrmuecke@gmail.com>
2024-01-02 18:17:53 -0500

    added the ability to roll multiple items

--- testFolder/test.js
+++ testFolder/test.js
@@ -1,155 +1,315 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Creating variables for the arrays to be used later as well as the json file where we get our data from
+  const magicItemsData = "test.json";
 
-document.addEventListener('DOMContentLoaded', function() {
-        
-    // Creating variables for the arrays to be used later as well as the json file where we get our data from
-    const magicItemsData = "test.json"
+  // button shake animation
+  var button = document.getElementById("button");
 
-    // This function creates an array from the json file based on the rarity chosen in the dropdown menu on the index page
-    // and then spits out a random magical item from that array. This button is called when the button is pressed.       
-    async function getMagicItems() {
-        // Hide searched items in case the search function was used and also show the random item display
-        document.getElementById("itemsList").className = "text hideItems";
-        document.getElementById("itemDisplay").className = "text showItems";
+  function startShake() {
+    button.classList.add("animate-shake");
 
-        const response = await fetch(magicItemsData);
-        const magicItems = await response.json();
+    setTimeout(function () {
+      button.classList.remove("animate-shake");
+    }, 1000);
+  }
 
-        // variables for dropdown selections
-        var raritySelect = document.getElementById("raritySelect").value;
-        var typeSelect = document.getElementById("typeSelect").value;
-        // empty array for use whenever a dropdown rarity or type is selected
-        var items = [];
+  setTimeout(startShake, 5000);
 
-        // if no dropdown is selected then pull an item from the entire list
-        if (raritySelect === "" && typeSelect === "") {
+  setInterval(startShake, 20000);
 
-            // Create a random number based on the length of the array
-            const randomNumber = Math.floor(Math.random() * magicItems.length + 1);
+  // This function creates an array from the json file based on the rarity chosen in the dropdown menu on the index page
+  // and then spits out a random magical item from that array. This function is called when the button is pressed.
+  async function getMagicItems() {
+    // Hide searched items in case the search function was used
+    document.getElementById("itemsList").className = "text hideItems";
+    // set random item display to no longer be hidden
+    var displays = document.getElementsByClassName("itemDisplay");
 
-            var randomItem = magicItems[randomNumber];
+    for (var i = 0; i < displays.length; i++) {
+      displays[i].classList.remove("hideItems");
+    }
 
-            var { name, desc, rarity, type } = randomItem;
+    const response = await fetch(magicItemsData);
+    const magicItems = await response.json();
+    const itemDisplays = document.querySelectorAll(".itemDisplay");
+    const multiplierSelect = document.getElementById("multiplier");
+    var selectedValue = multiplierSelect.value;
+    const multiplier = parseInt(selectedValue, 10);
 
-            document.getElementById("name").textContent = name
-            document.getElementById("rarity").textContent = rarity.toUpperCase();
-            document.getElementById("type").textContent = type.toUpperCase();
-            document.getElementById("desc").textContent = desc
+    // variables for dropdown selections
+    var raritySelect = document.getElementById("raritySelect").value;
+    var typeSelect = document.getElementById("typeSelect").value;
+    // empty array for use whenever a dropdown rarity or type is selected
+    var items = [];
+
+    itemDisplays.forEach((itemDisplay) => {
+      itemDisplay.querySelector(".name").textContent = "";
+      itemDisplay.querySelector(".rarity").textContent = "";
+      itemDisplay.querySelector(".type").textContent = "";
+      itemDisplay.querySelector(".desc").textContent = "";
+    });
+
+    for (let n = 0; n < multiplier && n < itemDisplays.length; n++) {
+      const itemDisplay = itemDisplays[n];
+      // if no dropdown is selected then pull an item from the entire list
+      if (raritySelect === "" && typeSelect === "") {
+        // Create a random number based on the length of the array
+        const randomNumber = Math.floor(Math.random() * magicItems.length + 1);
+
+        const randomItem = magicItems[randomNumber];
+
+        var { name, desc, rarity, type } = randomItem;
+
+        itemDisplay.querySelector(".name").textContent = name;
+        itemDisplay.querySelector(".rarity").textContent = rarity.toUpperCase();
+        itemDisplay.querySelector(".type").textContent = type.toUpperCase();
+        itemDisplay.querySelector(".desc").textContent = desc;
         // if a rarity is chosen but no type is chosen
-        } else if (raritySelect !== "" && typeSelect === "") {
-            for (var i = 0; i < magicItems.length; i++) {
-                if (magicItems[i].rarity === raritySelect) {
-                    items.push(magicItems[i]);
-                }
-            }
-            const randomNumber = Math.floor(Math.random() * items.length);
+      } else if (raritySelect !== "" && typeSelect === "") {
+        for (var i = 0; i < magicItems.length; i++) {
+          if (magicItems[i].rarity === raritySelect) {
+            items.push(magicItems[i]);
+          }
+        }
+        const randomNumber = Math.floor(Math.random() * items.length);
 
-            var randomItem = items[randomNumber];
-            var { name, desc, rarity, type } = randomItem;
+        var randomItem = items[randomNumber];
+        var { name, desc, rarity, type } = randomItem;
 
-            document.getElementById("name").textContent = name;
-            document.getElementById("rarity").textContent = rarity.toUpperCase();
-            document.getElementById("type").textContent = type.toUpperCase();
-            document.getElementById("desc").textContent = desc;
+        itemDisplay.querySelector(".name").textContent = name;
+        itemDisplay.querySelector(".rarity").textContent = rarity.toUpperCase();
+        itemDisplay.querySelector(".type").textContent = type.toUpperCase();
+        itemDisplay.querySelector(".desc").textContent = desc;
         // if no rarity is chosen but a type is chosen
-        } else if (raritySelect === "" && typeSelect !== "") {
-            for (var i = 0; i < magicItems.length; i++) {
-                if (magicItems[i].type === typeSelect) {
-                    items.push(magicItems[i]);
-                }
-            }
-            const randomNumber = Math.floor(Math.random() * items.length);
-
-            var randomItem = items[randomNumber];
-            var { name, desc, rarity, type } = randomItem;
-
-            document.getElementById("name").textContent = name;
-            document.getElementById("rarity").textContent = rarity.toUpperCase();
-            document.getElementById("type").textContent = type.toUpperCase();
-            document.getElementById("desc").textContent = desc;
-        // if a rarity and a type are chosen
-        } else {
-            for (var i = 0; i < magicItems.length; i++) {
-                if (magicItems[i].rarity === raritySelect && magicItems[i].type === typeSelect) {
-                    items.push(magicItems[i]);
-                }
-            }
-            const randomNumber = Math.floor(Math.random() * items.length);
-
-            var randomItem = items[randomNumber];
-            var { name, desc, rarity, type } = randomItem;
-
-            document.getElementById("name").textContent = name;
-            document.getElementById("rarity").textContent = rarity.toUpperCase();
-            document.getElementById("type").textContent = type.toUpperCase();
-            document.getElementById("desc").textContent = desc;
+      } else if (raritySelect === "" && typeSelect !== "") {
+        for (var i = 0; i < magicItems.length; i++) {
+          if (magicItems[i].type === typeSelect) {
+            items.push(magicItems[i]);
+          }
         }
-        var pulledRarity = document.getElementById("rarity").textContent;
-        switch(pulledRarity) {
-        case "COMMON" :
-            document.getElementById("name").style.color = "brown";
-            break;
-        case "UNCOMMON" :
-            document.getElementById("name").style.color = "orange";
-            break;
-        case "RARE" :
-            document.getElementById("name").style.color = "yellow";
-            break;
-        case "VERY RARE" :
-            document.getElementById("name").style.color = "green";
-            break;
-        case "LEGENDARY" :
-            document.getElementById("name").style.color = "blue";
-            break;
-        case "ARTIFACT" :
-            document.getElementById("name").style.color = "purple";
-            break;
-        default :
-            document.getElementById("name").style.color = "white";
+        const randomNumber = Math.floor(Math.random() * items.length);
+
+        var randomItem = items[randomNumber];
+        var { name, desc, rarity, type } = randomItem;
+
+        itemDisplay.querySelector(".name").textContent = name;
+        itemDisplay.querySelector(".rarity").textContent = rarity.toUpperCase();
+        itemDisplay.querySelector(".type").textContent = type.toUpperCase();
+        itemDisplay.querySelector(".desc").textContent = desc;
+        // if both a rarity and a type are chosen
+      } else {
+        for (var i = 0; i < magicItems.length; i++) {
+          if (
+            magicItems[i].rarity === raritySelect &&
+            magicItems[i].type === typeSelect
+          ) {
+            items.push(magicItems[i]);
+          }
         }
+        const randomNumber = Math.floor(Math.random() * items.length);
+
+        var randomItem = items[randomNumber];
+        var { name, desc, rarity, type } = randomItem;
+
+        itemDisplay.querySelector(".name").textContent = name;
+        itemDisplay.querySelector(".rarity").textContent = rarity.toUpperCase();
+        itemDisplay.querySelector(".type").textContent = type.toUpperCase();
+        itemDisplay.querySelector(".desc").textContent = desc;
+      }
+
+      // set the color of the item name based on rarity
+      var pulledRarity = itemDisplay.querySelector(".rarity").textContent;
+      switch (pulledRarity) {
+        case "COMMON":
+          itemDisplay.querySelector(".name").style.color = "brown";
+          break;
+        case "UNCOMMON":
+          itemDisplay.querySelector(".name").style.color = "orange";
+          break;
+        case "RARE":
+          itemDisplay.querySelector(".name").style.color = "yellow";
+          break;
+        case "VERY RARE":
+          itemDisplay.querySelector(".name").style.color = "green";
+          break;
+        case "LEGENDARY":
+          itemDisplay.querySelector(".name").style.color = "blue";
+          break;
+        case "ARTIFACT":
+          itemDisplay.querySelector(".name").style.color = "purple";
+          break;
+        default:
+          itemDisplay.querySelector(".name").style.color = "white";
+      }
     }
-    button.addEventListener("click", function() {
-        getMagicItems();
-    })  
+    itemDisplays.forEach((itemDisplay) => {
+      const rerollButton = itemDisplay.querySelector(".reroll");
+      if (itemDisplay.querySelector(".name").textContent !== "") {
+        rerollButton.classList.remove("hideButton");
+      } else {
+        rerollButton.classList.add("hideButton");
+      }
+    });
+  }
 
-    // search function
-    async function itemsList() {
-        const response = await fetch(magicItemsData);
-        const magicItems = await response.json();
+  // main button function call
+  button.addEventListener("click", function () {
+    getMagicItems();
+  });
 
-        var list = document.getElementById("itemsList");
-        for (var i = 0; i < magicItems.length; ++i) {
-            var li = document.createElement('li');
-            var itemNames = magicItems[i].name;
-            li.innerText = itemNames + ": " + magicItems[i].desc;
-            list.appendChild(li);
+  // reroll button
+  async function getMagicItem(itemDisplay) {
+    const response = await fetch(magicItemsData);
+    const magicItems = await response.json();
+
+    // variables for dropdown selections
+    var raritySelect = document.getElementById("raritySelect").value;
+    var typeSelect = document.getElementById("typeSelect").value;
+    // empty array for use whenever a dropdown rarity or type is selected
+    var items = [];
+
+    // if no dropdown is selected then pull an item from the entire list
+    if (raritySelect === "" && typeSelect === "") {
+      // Create a random number based on the length of the array
+      const randomNumber = Math.floor(Math.random() * magicItems.length + 1);
+
+      var randomItem = magicItems[randomNumber];
+
+      var { name, desc, rarity, type } = randomItem;
+
+      itemDisplay.querySelector(".name").textContent = name;
+      itemDisplay.querySelector(".rarity").textContent = rarity.toUpperCase();
+      itemDisplay.querySelector(".type").textContent = type.toUpperCase();
+      itemDisplay.querySelector(".desc").textContent = desc;
+      // if a rarity is chosen but no type is chosen
+    } else if (raritySelect !== "" && typeSelect === "") {
+      for (var i = 0; i < magicItems.length; i++) {
+        if (magicItems[i].rarity === raritySelect) {
+          items.push(magicItems[i]);
         }
-        document.getElementById("itemsList").className += " hideItems";
+      }
+      const randomNumber = Math.floor(Math.random() * items.length);
+
+      var randomItem = items[randomNumber];
+      var { name, desc, rarity, type } = randomItem;
+
+      itemDisplay.querySelector(".name").textContent = name;
+      itemDisplay.querySelector(".rarity").textContent = rarity.toUpperCase();
+      itemDisplay.querySelector(".type").textContent = type.toUpperCase();
+      itemDisplay.querySelector(".desc").textContent = desc;
+      // if no rarity is chosen but a type is chosen
+    } else if (raritySelect === "" && typeSelect !== "") {
+      for (var i = 0; i < magicItems.length; i++) {
+        if (magicItems[i].type === typeSelect) {
+          items.push(magicItems[i]);
+        }
+      }
+      const randomNumber = Math.floor(Math.random() * items.length);
+
+      var randomItem = items[randomNumber];
+      var { name, desc, rarity, type } = randomItem;
+
+      itemDisplay.querySelector(".name").textContent = name;
+      itemDisplay.querySelector(".rarity").textContent = rarity.toUpperCase();
+      itemDisplay.querySelector(".type").textContent = type.toUpperCase();
+      itemDisplay.querySelector(".desc").textContent = desc;
+      // if a rarity and a type are chosen
+    } else {
+      for (var i = 0; i < magicItems.length; i++) {
+        if (
+          magicItems[i].rarity === raritySelect &&
+          magicItems[i].type === typeSelect
+        ) {
+          items.push(magicItems[i]);
+        }
+      }
+      const randomNumber = Math.floor(Math.random() * items.length);
+
+      var randomItem = items[randomNumber];
+      var { name, desc, rarity, type } = randomItem;
+
+      itemDisplay.querySelector(".name").textContent = name;
+      itemDisplay.querySelector(".rarity").textContent = rarity.toUpperCase();
+      itemDisplay.querySelector(".type").textContent = type.toUpperCase();
+      itemDisplay.querySelector(".desc").textContent = desc;
     }
-    // Filters out the items based on what is typed in the search bar, it is called when a user begins typing in that bar.
-    document.getElementById("input").addEventListener("keyup", search);
-    function search() {
-        //clear random item display if there happens to be one
-        document.getElementById("itemDisplay").className = "text hideItems";
-
-        var input, filter, ul, li, i, txtValue;
-        input = document.getElementById("input");
-        filter = input.value.toUpperCase();
-        ul = document.getElementById("itemsList");
-        li = ul.getElementsByTagName("li");
-        ul.className = "showItems";
-
-        for (i = 0; i < li.length; i++) {
-            txtValue = li[i].textContent || li[i].innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                document.getElementById("itemsList").className = "text showItems";
-                li[i].style.display = "";
-            } else {
-                li[i].style.display = "none";
-            }
-        }
-        if (document.getElementById("input").value === "") {
-            document.getElementById("itemsList").className = "text hideItems";
-        }
+    var pulledRarity = itemDisplay.querySelector(".rarity").textContent;
+    switch (pulledRarity) {
+      case "COMMON":
+        itemDisplay.querySelector(".name").style.color = "brown";
+        break;
+      case "UNCOMMON":
+        itemDisplay.querySelector(".name").style.color = "orange";
+        break;
+      case "RARE":
+        itemDisplay.querySelector(".name").style.color = "yellow";
+        break;
+      case "VERY RARE":
+        itemDisplay.querySelector(".name").style.color = "green";
+        break;
+      case "LEGENDARY":
+        itemDisplay.querySelector(".name").style.color = "blue";
+        break;
+      case "ARTIFACT":
+        itemDisplay.querySelector(".name").style.color = "purple";
+        break;
+      default:
+        itemDisplay.querySelector(".name").style.color = "white";
     }
-    itemsList();
+  }
+
+  // reroll function call
+  document.querySelectorAll(".itemDisplay").forEach(function (itemDisplay) {
+    var rerollButton = itemDisplay.querySelector(".reroll");
+
+    rerollButton.addEventListener("click", function () {
+      getMagicItem(itemDisplay);
+    });
+  });
+
+  // search function
+  async function itemsList() {
+    const response = await fetch(magicItemsData);
+    const magicItems = await response.json();
+
+    var list = document.getElementById("itemsList");
+    for (var i = 0; i < magicItems.length; ++i) {
+      var li = document.createElement("li");
+      var itemNames = magicItems[i].name;
+      li.innerText = itemNames + ": " + magicItems[i].desc;
+      list.appendChild(li);
+    }
+    document.getElementById("itemsList").className += " hideItems";
+  }
+  // Filters out the items based on what is typed in the search bar, it is called when a user begins typing in that bar.
+  document.getElementById("input").addEventListener("keyup", search);
+  function search() {
+    //clear random item display if there happens to be one
+    var itemDisplays = document.getElementsByClassName("itemDisplay");
+
+    for (var i = 0; i < itemDisplays.length; i++) {
+      itemDisplays[i].classList.add("hideItems");
+    }
+
+    var input, filter, ul, li, i, txtValue;
+    input = document.getElementById("input");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("itemsList");
+    li = ul.getElementsByTagName("li");
+    ul.className = "showItems";
+
+    for (i = 0; i < li.length; i++) {
+      txtValue = li[i].textContent || li[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        document.getElementById("itemsList").className = "text showItems";
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+    if (document.getElementById("input").value === "") {
+      document.getElementById("itemsList").className = "text hideItems";
+    }
+  }
+  itemsList();
 });
